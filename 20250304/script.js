cb6375afa38d0107978592368ee980185cd6de1a
Franck <francklopez92@outlook.es>
2025-03-04 19:47:58 +0100

    Update javascript
    Intro javascript

---
+++ 20250304/script.js
@@ -0,0 +1,12 @@
+document.getElementById("sumarBtn").addEventListener("click",
+
+    function () {
+
+        let num1 = parseInt(document.getElementById("num1").value);
+
+        let num2 = parseInt(document.getElementById("num2").value);
+
+        let suma = num1 + num2;
+        document.getElementById("resultado").textContent = suma;
+
+    });
