cb0a5c12b2f12afc24353870ff236862a3950ab6
Franck <francklopez92@outlook.es>
2025-03-17 20:41:55 +0100

    Update 17.03

---
+++ 20250317/script.js
@@ -0,0 +1,6 @@
+document.getElementById("calcularBtn").addEventListener("click", function(){
+
+    let numero = parseFloat(document.getElementById("numero").value);
+    let porcentaje = parseFloat(document.getElementById("porcentaje").value);
+
+});
